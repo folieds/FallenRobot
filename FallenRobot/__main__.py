dcf8d614fc41d963cab2c648b03c6a6cbebab7b7
folieds <yosnkooke@gmail.com>
2024-09-04 22:06:48 +0530

    Update __main__.py
    Signed-off-by: folieds <yosnkooke@gmail.com>

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -400,7 +400,7 @@ def Fallen_about_callback(update: Update, context: CallbackContext):
                             text="sᴜᴩᴩᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"
                         ),
                         InlineKeyboardButton(
-                            text="ᴜᴩᴅᴀᴛᴇs", url=f"https://t.me/outlawbots"
+                            text="ᴜᴩᴅᴀᴛᴇs", url="https://t.me/outlawbots"
                         ),
                     ],
                     [
